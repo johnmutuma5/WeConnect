b4c15911f4bf57be1cabe0d5fb8381e160f9d390
John Mutuma <johnmutuma5@gmail.com>
2018-03-06 07:07:57 +0300

    remove GET method from API endpoint

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from .models import User, Business, Review
 from flask import jsonify, request
 import json
 
-@app.route('/api/v1/auth/register', methods = ['POST', 'GET'])
+@app.route('/api/v1/auth/register', methods = ['POST'])
 def register ():
     data = json.loads(request.data.decode('utf-8'))
     user = User.create_user (data)
