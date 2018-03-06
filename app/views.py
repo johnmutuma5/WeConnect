749c84b56dc29beb96fa76a10c152983281f67b9
John Mutuma <johnmutuma5@gmail.com>
2018-03-06 03:30:24 +0300

    Add URL version

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import jsonify
 
-@app.route('/api/auth/register', methods = ['POST'])
+@app.route('/api/v1/auth/register', methods = ['POST'])
 def register ():
     return jsonify ('connected')
--- run.py
+++ run.py
@@ -1,8 +1,5 @@
 #! venv/bin/python
-
-
 from app import app
-
 
 if __name__ == '__main__':
     from werkzeug.serving import run_simple
