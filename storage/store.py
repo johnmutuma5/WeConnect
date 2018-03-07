43e5d56c8f0b9c52d9353cb820f7fc4757c0c9d9
John Mutuma <johnmutuma5@gmail.com>
2018-03-07 05:42:29 +0300

    Enable adding a business
    CHANGES:
    - Create a method add_business of the Store class to add business
    - Create a method add of the Store class to add any type of object to the store

--- storage/store.py
+++ storage/store.py
@@ -3,18 +3,37 @@ from app.exceptions import DuplicationError
 class Storage ():
     '''
     Simulates a database:
+    Implemented with dicts for constant time access to data
         params: None
         fields:
-            users: a dict of users (key:value) equivalent (username: user)
+            users: a dict of users (key:value) equivalent (username: user_obj)
+            businesses: a dict o businesses (key:value) equivalent (businessname: business_obj)
         methods:
+            add: adds any class of object
+                params: obj
             add_user:
                 params: user_obj
+            add_business:
+                params: business_obj
 
     '''
     users = {}
+    business = {}
 
     def __init__ (self):
         ...
+
+    def add (self, obj):
+        obj_class = obj.__class__.__name__
+
+        # get appropriate method to call: a switch-like dict operation
+        _add = {
+            "User": self.add_user,
+            "Business": self.add_business
+        }[obj_class]
+
+        # call method with argument
+        _add (obj)
 
     def add_user (self, user_obj):
         users = self.__class__.users
@@ -24,4 +43,14 @@ class Storage ():
 
         self.__class__.users[username] = user_obj
         new_user = self.__class__.users[username]
+        return 'SUCCESS: user {} created!'.format(new_user.username)
+
+    def add_business (self, business_obj):
+        businesses = self.__class__.businesses
+        businessname = business_obj.name
+
+        if businesses.get(businessname): raise DuplicationError ('Storage::add_user', 'Duplicate username not allowed')
+
+        self.__class__.businesses[businessname] = business_obj
+        new_business = self.__class__.businesses[businessname]
         return 'SUCCESS: user {} created!'.format(new_user.username)
