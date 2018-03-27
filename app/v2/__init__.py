378479df539fa075044a876f5c78e19a82f1447e
John Mutuma <johnmutuma5@gmail.com>
2018-03-27 14:54:42 +0300

    Create a database engine

--- app/v2/__init__.py
+++ app/v2/__init__.py
@@ -1,8 +1,14 @@
 from flask import Blueprint
 from .storage import Storage
+from sqlalchemy import create_engine
 
 v2 = Blueprint ('v2', __name__)
-store = Storage ()
+
+#import app here to avoid cyclical imports
+from app import app
+
+dbEngine = create_engine (app.config['SQLALCHEMY_DATABASE_URI'])
+store = Storage (dbEngine)
 
 # from . import views
 from app import common_views
--- config.py
+++ config.py
@@ -3,3 +3,11 @@ import os
 class Config():
     SECRET_KEY = os.urandom(24)
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:databasepass@localhost/weconnect"
+
+class ProductionConfig (Config):
+    DEBUG = False
+    TESTING = False
+
+class DevelopmentConfig (Config):
+    DEBUG = True
+    TESTING = True
