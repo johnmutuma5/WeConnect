498881984ede6ae432173675576cfa2263abae22
John Mutuma <johnmutuma5@gmail.com>
2018-03-29 18:40:04 +0300

    Add test users can reset password

--- tests/test_api.py
+++ tests/test_api.py
@@ -259,32 +259,33 @@ class TestAPICase (BaseAPITestSetUp):
         # check that all review heading have been returned
         for data in review_data:
             self.assertIn (data['heading'], resp_review_headings)
-#
-#     def test_users_can_reset_password (self):
-#         self.testHelper.register_user (user_data)
-#         # use username to send password reset request
-#         username = user_data['username']
-#         reset_data = {"username": username}
-#         resp = self.testHelper.reset_password (reset_data)
-#         token = (json.loads(resp.data.decode ('utf-8')))['t']
-#         # supply a new password
-#         reset_data = {'new_password': "changed"}
-#         resp = self.testHelper.reset_password (reset_data, token)
-#         msg = (json.loads (resp.data.decode ('utf-8')))['msg']
-#         self.assertEqual (msg, "Password updated successfully")
-#         # login with new password
-#         resp = self.testHelper.login_user ({"username": username,
-#                                         "password": "changed"})
-#         msg = (json.loads (resp.data.decode('utf-8')))['msg']
-#         pattern = r"Logged in (?P<username>.+)"
-#         self.assertRegexpMatches (msg, pattern)
-#         # test users cannot use an invalid token
-#         token = r"aquitelongstringrepresentingatokentoresetpassword"
-#         resp = self.testHelper.reset_password (reset_data, token)
-#         msg = (json.loads (resp.data.decode ('utf-8')))['msg']
-#         self.assertEqual (msg, "Invalid token")
-#
-#
+
+
+    def test_users_can_reset_password (self):
+        self.testHelper.register_user (user_data)
+        # use username to send password reset request
+        username = user_data['username']
+        reset_data = {"username": username}
+        resp = self.testHelper.reset_password (reset_data)
+        token = (json.loads(resp.data.decode ('utf-8')))['t']
+        # supply a new password
+        reset_data = {'new_password': "changed"}
+        resp = self.testHelper.reset_password (reset_data, token)
+        msg = (json.loads (resp.data.decode ('utf-8')))['msg']
+        self.assertEqual (msg, "Password updated successfully")
+        # login with new password
+        resp = self.testHelper.login_user ({"username": username,
+                                        "password": "changed"})
+        msg = (json.loads (resp.data.decode('utf-8')))['msg']
+        pattern = r"Logged in (?P<username>.+)"
+        self.assertRegexpMatches (msg, pattern)
+        # test users cannot use an invalid token
+        token = r"aquitelongstringrepresentingatokentoresetpassword"
+        resp = self.testHelper.reset_password (reset_data, token)
+        msg = (json.loads (resp.data.decode ('utf-8')))['msg']
+        self.assertEqual (msg, "Invalid token")
+
+
 
 #
 
