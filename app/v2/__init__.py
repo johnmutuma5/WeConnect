cc5c52a544ad905d35d080f76e87f03e64ffb432
John Mutuma <johnmutuma5@gmail.com>
2018-03-31 08:52:42 +0300

    Import config: to retrieve Db URI

--- app/v2/__init__.py
+++ app/v2/__init__.py
@@ -4,10 +4,9 @@ from sqlalchemy import create_engine
 
 v2 = Blueprint('v2', __name__)
 
-# import app here to avoid cyclical imports
-from app import app
-
-dbEngine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+# import config here to avoid cyclical imports
+from app import config
+dbEngine = create_engine(config['SQLALCHEMY_DATABASE_URI'])
 store = DbInterface(dbEngine)
 
 # from . import views
