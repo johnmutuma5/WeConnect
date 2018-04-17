2b93278a5a1c1c2fe09b75e1cc1159783e771d5b
John Mutuma <johnmutuma@Johns-MacBook-Pro.local>
2018-04-17 21:54:23 +0300

    Add test database

--- config.py
+++ config.py
@@ -3,16 +3,17 @@ import os
 
 class Config():
     SECRET_KEY = os.urandom(24)
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:databasepass@localhost/weconnect"
 
 
 class ProductionConfig (Config):
     DEBUG = False
     TESTING = False
     PASSWORD_RESET_TOKEN_LIFETIME = {'hours':24}
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:databasepass@localhost/weconnect"
 
 
 class DevelopmentConfig (Config):
     DEBUG = True
     TESTING = True
     PASSWORD_RESET_TOKEN_LIFETIME = {'seconds': 1}
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:databasepass@localhost/testdb"
