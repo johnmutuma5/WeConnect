28bcb9946144ea1550f9aa4b1b02d24c0f766e69
John Mutuma <johnmutuma5@gmail.com>
2018-04-29 09:53:46 +0300

    [bg-random-range] Fix random number range to include alphanumeric range

--- app/helpers.py
+++ app/helpers.py
@@ -1,17 +1,16 @@
 import random
 from .exceptions import MissingDataError
 
-
-def generate_token():
+def generate_token ():
     chars = ""
     for i in range(26):
-        chars += chr(65+i)
-        chars += chr(97+i)
+        chars += chr (65+i)
+        chars += chr (97+i)
         if i > 9:
             continue
         chars += str(i)
 
-    token_chars = []
+    token = ""
     for i in range(96):
         rand_index = random.randint(0, 61)
         token += chars[rand_index]
