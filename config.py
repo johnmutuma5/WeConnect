c676fe23855c632daa543d1958d4ba2abb690b60
John Mutuma <johnmutuma5@gmail.com>
2018-05-09 08:32:13 +0300

    [Chore #] Introduce database contraints naming convention

--- config.py
+++ config.py
@@ -6,6 +6,14 @@ class Config():
     TESTING = True
     SECRET_KEY = 'asecretkeyfortheapi'
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:databasepass@localhost:5432/testdb"
+    NAMING_CONVENTION = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    }
+    NAMING_CONVENTION_REGEX = r"\"\w{2,}(_(?P<table>.+))?_(?P<column>.+)(_(?P<foreign_table>.+))?\""
 
 
 class ProductionConfig (Config):
