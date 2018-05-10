6df4d7ebae99a9baf97e204b7115468cca74e5ca
John Mutuma <johnmutuma5@gmail.com>
2018-05-10 19:13:30 +0300

    [Chore #155524156] Add tests
    - Test filters, pagination and search

--- app/business/tests/test_get_info.py
+++ app/business/tests/test_get_info.py
@@ -50,10 +50,60 @@ class TestBusinessCase(BaseAPITestSetUp):
             "location": "Nairobi",
             "category": "Technology"
         }
-        resp = self.testHelper.search_business(filter_dict)
+        resp = self.testHelper.filter_business(filter_dict)
         results = (json.loads(resp.data.decode('utf-8')))['results']
         for result in results:
             search_key = filter_dict['name'].lower()
+            business_name = (result['name']).lower()
+            self.assertTrue(search_key in business_name)
+
+    def test_filter_applies_pagination (self):
+        self.testHelper.register_user(user_data)
+        res = self.testHelper.login_user(login_data)
+        access_token = (json.loads(res.data.decode("utf-8")))['access_token']
+        for business_data in businesses_data:
+            self.testHelper.register_business(business_data, access_token)
+        # define a search key
+        limit = 2
+        filter_dict = {
+            "location": "nairobi",
+            "limit": limit,
+            "page": 1
+        }
+        resp = self.testHelper.filter_business(filter_dict)
+        results = (json.loads(resp.data.decode('utf-8')))['results']
+        self.assertTrue(len(results) == limit)
+
+
+    def test_it_handles_filter_with_invalid_limit_or_page(self):
+        self.testHelper.register_user(user_data)
+        res = self.testHelper.login_user(login_data)
+        access_token = (json.loads(res.data.decode("utf-8")))['access_token']
+        for business_data in businesses_data:
+            self.testHelper.register_business(business_data, access_token)
+        # define a search key
+        filter_dict = {
+            "name": "Google",
+            "limit": 'ten',
+            "page": 1
+        }
+        resp = self.testHelper.filter_business(filter_dict)
+        msg = (json.loads(resp.data.decode('utf-8')))['msg']
+        self.assertEqual(msg, 'Invalid pagination limit or page')
+
+
+    def test_users_can_search_for_business(self):
+        self.testHelper.register_user(user_data)
+        res = self.testHelper.login_user(login_data)
+        access_token = (json.loads(res.data.decode("utf-8")))['access_token']
+        for business_data in businesses_data:
+            self.testHelper.register_business(business_data, access_token)
+        # define a search key
+        search_key = 'lE'
+        resp = self.testHelper.search_business(search_key)
+        results = (json.loads(resp.data.decode('utf-8')))['results']
+        for result in results:
+            search_key = search_key.lower()
             business_name = (result['name']).lower()
             self.assertTrue(search_key in business_name)
 
