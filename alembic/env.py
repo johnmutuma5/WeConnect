ad0ab8950a80dcf1f8264128b91e730f4629459c
John Mutuma <johnmutuma5@gmail.com>
2018-05-12 17:14:55 +0300

    [Chore] Make DATABASE_URL dynamic

--- alembic/env.py
+++ alembic/env.py
@@ -2,10 +2,12 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
