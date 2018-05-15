202613228e1efa1f27f7d8c7f2f32e233c5d6f38
John Mutuma <johnmutuma5@gmail.com>
2018-05-15 05:10:16 +0300

    [Chore] Add API Blueprint

---
+++ APIBlueprint/genHTML.js
@@ -0,0 +1,17 @@
+let aglio = require('aglio');
+
+// Render blueprint to html
+
+const options = {
+  themeVariables: 'default'
+};
+
+
+const documentation_output = './designs/UI/documentation.html';
+const documentation_input = './APIBlueprint/blueprint.md';
+
+aglio.renderFile(documentation_input, documentation_output, options, function (err, warnings) {
+    if (err) return console.log(err);
+    // if (warnings) console.log(warnings);
+    console.log('documentation.html successfully dumped in ' + documentation_output);
+});
